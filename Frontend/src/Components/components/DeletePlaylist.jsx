55a1f43673504867beec6116b930e72a71110b57
unknown <maja.geo21@gmail.com>
2021-09-30 19:32:53 +0200

    delete playlist update not working

---
+++ Frontend/src/Components/components/DeletePlaylist.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ACCESS_TOKEN_NAME, API_BASE_URL } from '../../Constants/apiContants'
+import axios from 'axios'
+
+
+function DeletePlaylist({id}) {
+
+function removePlaylist() {
+
+    axios({
+        method: "DELETE",
+        url: API_BASE_URL + "playlist",
+        data: { playlistId: id },
+        headers: {
+          "Content-type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem(ACCESS_TOKEN_NAME),
+        },
+      }).then((response) => { 
+          console.log(response,'playlist response');
+    });
+}
+    //render()
+    return (
+        <button className="btn btn-sm btn-danger" onClick={removePlaylist} >
+        <i className="fa fa-trash"></i>
+        </button>
+    )
+}
+
+
+export default DeletePlaylist
